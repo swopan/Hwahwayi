c9bb73f50def6895aa30e067bb61ce2aeca828a0
swopan <38856499+swopan@users.noreply.github.com>
2024-03-18 14:38:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1040980316"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://swopandeep7:Batala321@swopan.44qqnn7.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://gguhgggh440:Batala123@swopan.ski9cxk.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Swopan")
 
 #force sub channel id, if you want enable force sub
